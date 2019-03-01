2517a76196a419777595aa3a2673ebddf915875e
EreenyEssam <minaFSedrak55.gmail.com>
2019-03-01 11:58:47 +0200

    set confiq

--- index.js
+++ index.js
@@ -9,14 +9,14 @@ import { Container } from './src/naviagation/index';
 
 import * as firebase from 'firebase';
 
- config = {
+var config = {
     apiKey: "AIzaSyC36MsoCq_Ef8q5X361HmSgtws10dmefCg",
     authDomain: "graduationpoject-38b49.firebaseapp.com",
     databaseURL: "https://graduationpoject-38b49.firebaseio.com",
     projectId: "graduationpoject-38b49",
     storageBucket: "graduationpoject-38b49.appspot.com",
     messagingSenderId: "919023476235"
-  };
-  firebase.initializeApp(config);
+};
+firebase.initializeApp(config);
 
 AppRegistry.registerComponent(appName, () => Container);
