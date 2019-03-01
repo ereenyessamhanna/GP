c5a538f107df148bf3f55b9029e713643a3166ea
MinaFSedrak55 <minaFSedrak55.gmail.com>
2019-03-01 11:40:58 +0200

    add firebase config

--- index.js
+++ index.js
@@ -7,4 +7,16 @@ import { AppRegistry } from 'react-native';
 import { name as appName } from './app.json';
 import { Container } from './src/naviagation/index';
 
+import * as firebase from 'firebase';
+
+ config = {
+    apiKey: "AIzaSyC36MsoCq_Ef8q5X361HmSgtws10dmefCg",
+    authDomain: "graduationpoject-38b49.firebaseapp.com",
+    databaseURL: "https://graduationpoject-38b49.firebaseio.com",
+    projectId: "graduationpoject-38b49",
+    storageBucket: "graduationpoject-38b49.appspot.com",
+    messagingSenderId: "919023476235"
+  };
+  firebase.initializeApp(config);
+
 AppRegistry.registerComponent(appName, () => Container);
